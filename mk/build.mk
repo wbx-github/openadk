wbx-github/openadk

commit e48e15bff425241ec65f5f892cfd1d2c17a19d67

mk: Introduce KERNEL_MAKE variable
This combines the typical kernel make call idiom into a single variable
reference. A side-effect is that parallel make is enabled (via added
'-j' flag) in many cases, but that shouldn't cause harm.

Signed-off-by: Phil Sutter <phil@nwl.cc>

mk/build.mk
@@ -232,10 +232,7 @@ targethelp:
 	$(MAKE) -C target targethelp 
 
 kernelconfig:
-	${KERNEL_MAKE_ENV} ${MAKE} -C "${LINUX_DIR}" \
-		ARCH=$(ADK_TARGET_KARCH) \
-		${KERNEL_MAKE_OPTS} \
-		menuconfig
+	${KERNEL_MAKE} menuconfig
 
 ifeq ($(ADK_TARGET_LINUX_KERNEL_USE_CUSTOMCONFIG),y)
 savekconfig:

mk/image.mk
@@ -313,8 +313,7 @@ ifeq ($(ADK_LINUX_KERNEL_COMPRESS_NONE),y)
 		echo "CONFIG_RD_ZSTD=n" >> ${LINUX_DIR}/.config
 endif
 	@-rm $(LINUX_DIR)/usr/initramfs_data.cpio* 2>/dev/null
-	env $(KERNEL_MAKE_ENV) $(MAKE) -C "${LINUX_DIR}" $(KERNEL_MAKE_OPTS) \
-		-j${ADK_MAKE_JOBS} $(ADK_TARGET_KERNEL) $(MAKE_TRACE)
+	${KERNEL_MAKE} $(ADK_TARGET_KERNEL) $(MAKE_TRACE)
 	@cp $(KERNEL) $(FW_DIR)/$(TARGET_KERNEL)
 
 ${FW_DIR}/${ROOTFSISO}: ${TARGET_DIR} kernel-package

mk/kernel-build.mk
@@ -72,23 +72,23 @@ $(LINUX_DIR)/.config: $(LINUX_DIR)/.prepared
 	$(START_TRACE) "target/$(ADK_TARGET_ARCH)-kernel-configure.. "
 	echo "-${KERNEL_RELEASE}" >${LINUX_DIR}/localversion
 ifeq ($(ADK_TARGET_LINUX_KERNEL_USE_DEFCONFIG),y)
-	${KERNEL_MAKE_ENV} $(MAKE) -C "${LINUX_DIR}" ${KERNEL_MAKE_OPTS} $(ADK_TARGET_LINUX_KERNEL_DEFCONFIG) $(MAKE_TRACE)
+	${KERNEL_MAKE} $(ADK_TARGET_LINUX_KERNEL_DEFCONFIG) $(MAKE_TRACE)
 else ifeq ($(ADK_TARGET_LINUX_KERNEL_USE_CUSTOMCONFIG),y)
 	@if [ ! -f $(ADK_TOPDIR)/$(ADK_TARGET_LINUX_KERNEL_CUSTOMCONFIG_PATH) ];then \
 		echo "no kernel configuration found in $(ADK_TOPDIR)/$(ADK_TARGET_LINUX_KERNEL_CUSTOMCONFIG_PATH)"; \
 		exit 1; \
 	fi
-	${KERNEL_MAKE_ENV} $(MAKE) -C "${LINUX_DIR}" ${KERNEL_MAKE_OPTS} KCONFIG_ALLCONFIG=$(ADK_TOPDIR)/$(ADK_TARGET_LINUX_KERNEL_CUSTOMCONFIG_PATH) allnoconfig $(MAKE_TRACE)
+	${KERNEL_MAKE} KCONFIG_ALLCONFIG=$(ADK_TOPDIR)/$(ADK_TARGET_LINUX_KERNEL_CUSTOMCONFIG_PATH) allnoconfig $(MAKE_TRACE)
 else
 	$(CP) $(BUILD_DIR)/.kernelconfig $(LINUX_DIR)/mini.config
-	${KERNEL_MAKE_ENV} $(MAKE) -C "${LINUX_DIR}" ${KERNEL_MAKE_OPTS} KCONFIG_ALLCONFIG=mini.config allnoconfig $(MAKE_TRACE)
+	${KERNEL_MAKE} KCONFIG_ALLCONFIG=mini.config allnoconfig $(MAKE_TRACE)
 endif
 	$(CMD_TRACE) " done"
 	$(END_TRACE)
 
 $(LINUX_DIR)/$(KERNEL_FILE): $(LINUX_DIR)/.config
 	$(START_TRACE) "target/$(ADK_TARGET_ARCH)-kernel-compile.. "
-	${KERNEL_MAKE_ENV} $(MAKE) -C "${LINUX_DIR}" ${KERNEL_MAKE_OPTS} -j${ADK_MAKE_JOBS} $(KERNEL_TARGET) $(MAKE_TRACE)
+	${KERNEL_MAKE}  $(KERNEL_TARGET) $(MAKE_TRACE)
 	$(CMD_TRACE) " done"
 	$(END_TRACE)
 
@@ -98,8 +98,7 @@ install:
 ifneq ($(KERNEL_MODULES_USED),)
 	$(START_TRACE) "target/$(ADK_TARGET_ARCH)-kernel-modules-install.. "
 	rm -rf $(LINUX_BUILD_DIR)/modules
-	${KERNEL_MAKE_ENV} $(MAKE) -C "${LINUX_DIR}" ${KERNEL_MAKE_OPTS} \
-		DEPMOD=$(ADK_DEPMOD) \
+	${KERNEL_MAKE} DEPMOD=$(ADK_DEPMOD) \
 		INSTALL_MOD_PATH=$(LINUX_BUILD_DIR)/modules \
 		modules_install $(MAKE_TRACE)
 	$(CMD_TRACE) " done"

mk/kernel-vars.mk
@@ -35,3 +35,6 @@ endif
 
 KERNEL_MAKE_ENV:=	PATH="${TOOLCHAIN_DIR}/usr/bin:${STAGING_HOST_DIR}/usr/bin:$$PATH"
 LINUX_BUILD_DIR:=	$(BUILD_DIR)/$(ADK_TARGET_OS)-$(ADK_TARGET_ARCH)
+
+# wrapper to simplify calling make in kernel tree
+KERNEL_MAKE:=		env $(KERNEL_MAKE_ENV) $(MAKE) -C "${LINUX_DIR}" $(KERNEL_MAKE_OPTS) -j${ADK_MAKE_JOBS}

package/glibc/Makefile
@@ -113,11 +113,7 @@ endif
 		cd $(IDIR_GLIBC_DEV)/$(ADK_TARGET_LIBC_PATH); ln -sf $$file-$(PKG_GLIBCVER).so $$file.so; \
 	done
 	# header package
-	${KERNEL_MAKE_ENV} \
-	$(MAKE) -C $(TOOLCHAIN_BUILD_DIR)/w-linux-$(KERNEL_FILE_VER)-1/linux-$(KERNEL_FILE_VER) \
-		$(KERNEL_MAKE_OPTS) \
-		INSTALL_HDR_PATH=$(IDIR_GLIBC_DEV)/usr \
-		headers_install
+	${KERNEL_MAKE} INSTALL_HDR_PATH=$(IDIR_GLIBC_DEV)/usr headers_install
 	(cd $(WRKBUILD); \
 		$(GLIBC_ENV) \
 		$(TOOLCHAIN_BUILD_DIR)/w-$(PKG_NAME)-$(PKG_VERSION)-$(PKG_RELEASE)/$(PKG_NAME)-$(PKG_VERSION)/configure \

package/musl/Makefile
@@ -48,11 +48,7 @@ endif
 musl-dev-install:
 	${INSTALL_DIR} ${IDIR_MUSL_DEV}/usr/lib 
 	${CP} ${STAGING_TARGET_DIR}/usr/lib/crt*.o ${IDIR_MUSL_DEV}/usr/lib
-	${KERNEL_MAKE_ENV} \
-	$(MAKE) -C $(TOOLCHAIN_BUILD_DIR)/w-linux-$(KERNEL_VERSION)/linux-$(KERNEL_FILE_VER) \
-		$(KERNEL_MAKE_OPTS) \
-		INSTALL_HDR_PATH=$(IDIR_MUSL_DEV)/usr \
-		headers_install
+	${KERNEL_MAKE} INSTALL_HDR_PATH=$(IDIR_MUSL_DEV)/usr headers_install
 	$(MAKE) -C $(TOOLCHAIN_BUILD_DIR)/w-$(PKG_NAME)-$(PKG_VERSION)-$(PKG_RELEASE)/${PKG_NAME}-${PKG_VERSION} \
 		DESTDIR=$(IDIR_MUSL_DEV) \
 		install-headers

package/perf/Makefile
@@ -28,12 +28,10 @@ BUILD_STYLE:=		manual
 INSTALL_STYLE:=		manual
 
 do-build:
-	$(KERNEL_MAKE_ENV) $(MAKE) -C "${LINUX_DIR}" \
-		$(KERNEL_MAKE_OPTS) WERROR=0 -C "$(LINUX_DIR)/tools/perf" all
+	${KERNEL_MAKE} WERROR=0 -C "$(LINUX_DIR)/tools/perf" all
 
 do-install:
-	$(KERNEL_MAKE_ENV) $(MAKE) -C "${LINUX_DIR}" \
-		$(KERNEL_MAKE_OPTS) WERROR=0 -C "$(LINUX_DIR)/tools/perf" \
+	${KERNEL_MAKE} WERROR=0 -C "$(LINUX_DIR)/tools/perf" \
 		prefix="$(WRKINST)" install
 
 perf-install:

package/uclibc-ng/Makefile
@@ -49,11 +49,7 @@ uclibc-ng-dev-install:
 	-${CP} ${STAGING_TARGET_DIR}/usr/lib/libc.so ${IDIR_UCLIBC_NG_DEV}/usr/lib
 	-${CP} ${STAGING_TARGET_DIR}/usr/lib/uclibc_nonshared.a ${IDIR_UCLIBC_NG_DEV}/usr/lib
 	${CP} ${STAGING_TARGET_DIR}/usr/lib/crt*.o ${IDIR_UCLIBC_NG_DEV}/usr/lib
-	${KERNEL_MAKE_ENV} \
-	$(MAKE) -C $(TOOLCHAIN_BUILD_DIR)/w-linux-$(KERNEL_FILE_VER)-1/linux-$(KERNEL_FILE_VER) \
-		$(KERNEL_MAKE_OPTS) \
-		INSTALL_HDR_PATH=$(IDIR_UCLIBC_NG_DEV)/usr \
-		headers_install
+	${KERNEL_MAKE} INSTALL_HDR_PATH=$(IDIR_UCLIBC_NG_DEV)/usr headers_install
 	$(MAKE) -C $(TOOLCHAIN_BUILD_DIR)/w-$(PKG_NAME)-$(PKG_VERSION)-$(PKG_RELEASE)/${PKG_NAME}-${PKG_VERSION} \
 		PREFIX=$(IDIR_UCLIBC_NG_DEV)/ \
 		DEVEL_PREFIX=/usr \

commit 96c1640d7135e0a3c307effed15f9123d8a82bd0

Fix 'make kernelconfig'
Using KERNEL_MAKE variable in this spot breaks things, for some reason
linking fails if KERNEL_MAKE_ENV is passed to make. Before, this worked
by accident because due to missing kernel-vars.mk include,
KERNEL_MAKE_ENV evaluated empty.

Fixes: e48e15bff4252 ("mk: Introduce KERNEL_MAKE variable")
Signed-off-by: Phil Sutter <phil@nwl.cc>

mk/build.mk
@@ -234,7 +234,9 @@ targethelp:
 	$(MAKE) -C target targethelp 
 
 kernelconfig:
-	${KERNEL_MAKE} menuconfig
+	${MAKE} -C "${LINUX_DIR}" \
+		ARCH=$(ADK_TARGET_KARCH) \
+		menuconfig
 
 ifeq ($(ADK_TARGET_LINUX_KERNEL_USE_CUSTOMCONFIG),y)
 savekconfig: