853337b419c36b856c80ba86245f6fd0323a39a4
Waldemar Brodkorb <wbx@openadk.org>
2022-01-25 00:19:46 +0100

    reorder TARGET_CC workarounds

--- mk/kernel-vars.mk
+++ mk/kernel-vars.mk
@@ -1,20 +1,21 @@
 # This file is part of the OpenADK project. OpenADK is copyrighted
 # material, please see the LICENCE file in the top-level directory.
 
-KERNEL_MAKE_OPTS:=	V=1 \
+KERNEL_MAKE_OPTS:=	CC="$(TARGET_CC)"
+ifeq ($(ADK_TARGET_BINFMT_FDPIC),y)
+KERNEL_MAKE_OPTS:=	CC="$(TARGET_CC) -mno-fdpic"
+endif
+ifeq ($(ADK_TARGET_ARCH_MICROBLAZE),y)
+KERNEL_MAKE_OPTS:=	CC="$(TARGET_CC) -fno-tree-loop-distribute-patterns"
+endif
+
+KERNEL_MAKE_OPTS+=	V=1 \
 			ARCH="$(ADK_TARGET_KARCH)" \
 			CROSS_COMPILE="$(TARGET_CROSS)" \
 			HOSTCC="${HOST_CC}" \
 			HOSTCXX="${HOST_CXX}" \
 			SHELL='${SHELL}' \
 			CONFIG_SHELL='${SHELL}'
-
-ifeq ($(ADK_TARGET_BINFMT_FDPIC),y)
-KERNEL_MAKE_OPTS+=	CC="$(TARGET_CC) -mno-fdpic"
-else
-KERNEL_MAKE_OPTS+=	CC="$(TARGET_CC) -fno-tree-loop-distribute-patterns"
-endif
-
 
 # regex for relocs needs pcre
 ifeq ($(OS_FOR_BUILD),Darwin)
